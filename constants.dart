8471bf40ecda57a1fd51bd788499a803c271806f
Harshavardhan Vyas <110585769+HARSHAVARDHAN2907@users.noreply.github.com>
2023-10-16 17:56:40 +0530

    Update constants.dart

--- constants.dart
+++ constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const int appId = 1753094983;
+  static const int appId = 00;
   static const String appSign =
-      "8b785ea96610521b2fad44d1df3ca62c56cbe073bee588817baf2836433aaee7";
+      "";
 }
